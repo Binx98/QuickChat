ec4df7d6b628ede565d0332f4c7ac6d3219362bf
徐志斌 <1262254123@qq.com>
2023-11-12 12:26:24 +0800

    SaltChat咸聊项目搭建

---
+++ README.md
@@ -0,0 +1,31 @@
+#### 技术栈
+
+后端：
+
+1. JDK 8
+2. SpringBoot
+3. MySQL
+4. MyBatis-Plus
+5. Redis
+6. Redisson
+7. Netty
+8. Kafka
+9. ElasticSearch
+10. MinIO
+12. Sharding-JDBC
+13. ip2region
+14. HuTool
+15. JWT
+
+
+
+前端：兼容移动端（重要）
+
+1. HTML
+2. CSS
+3. JavaScript
+4. Vue.js
+5. Node.js
+6. ElementUI
+7. Axios
+8. npm
---
+++ saltchat-websocket/src/main/java/com/saltchat/saltchatwebsocket/SaltChatWebsocketApplication.java
@@ -0,0 +1,18 @@
+package com.saltchat.saltchatwebsocket;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaltChatWebsocketApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaltChatWebsocketApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
