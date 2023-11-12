c9673e823f713d59a4cb34279227f6d54315b705
徐志斌 <1262254123@qq.com>
2023-11-12 11:54:07 +0800

    SaltChat

---
+++ saltchat-websocket/src/main/java/com/saltchat/saltchatwebsocket/SaltchatWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.saltchat.saltchatwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaltchatWebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaltchatWebsocketApplication.class, args);
+    }
+
+}
