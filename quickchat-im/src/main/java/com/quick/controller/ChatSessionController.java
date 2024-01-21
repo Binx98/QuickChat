78bfaeb713ef5018f0f259bd168d442e3bc7bfba
徐志斌 <1262254123@qq.com>
2024-01-21 17:21:12 +0800

    fix：调整yml配置

--- quickchat-im/src/main/java/com/quick/controller/ChatSessionController.java
+++ quickchat-im/src/main/java/com/quick/controller/ChatSessionController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.concurrent.ExecutionException;
 
 /**
  * @Author 徐志斌
