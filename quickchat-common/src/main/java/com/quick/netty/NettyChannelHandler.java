cfbec4a5f45d8c322198e498c052e5b9646040f2
徐志斌 <1262254123@qq.com>
2024-01-24 09:10:31 +0800

    refactor：自定义Channel功能调整

--- quickchat-common/src/main/java/com/quick/netty/NettyChannelHandler.java
+++ quickchat-common/src/main/java/com/quick/netty/NettyChannelHandler.java
@@ -4,12 +4,7 @@ import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
-import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
-import io.netty.handler.timeout.IdleState;
-import io.netty.handler.timeout.IdleStateEvent;
-import io.netty.util.AttributeKey;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.stereotype.Component;
 
 /**
@@ -31,42 +26,39 @@ public class NettyChannelHandler extends SimpleChannelInboundHandler<TextWebSock
     }
 
     /**
+     * 建立连接
+     */
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) {
+        log.info("--------------WebSocket连接建立：{}--------------", ctx);
+    }
+
+    /**
      * 断开连接
      */
     @Override
     public void handlerRemoved(ChannelHandlerContext ctx) {
-        // 移除 Channel Group 中的 Channel 信息
-        UserChannelRelation.getChannelGroup().remove(ctx.channel());
-
-        // 移除 uid 和 Channel 关联关系
-        AttributeKey<Long> key = AttributeKey.valueOf("uid");
-        Long uid = ctx.channel().attr(key).get();
-        if (ObjectUtils.isNotEmpty(uid)) {
-            UserChannelRelation.getUserChannelMap().remove(uid);
-        }
-
-        // 用户状态更新为下线
-        log.info("------------------客户端与WebSocket服务器断开连接：{}------------------", ctx);
+        log.info("--------------WebSocket连接断开：{}--------------", ctx);
     }
 
     /**
      * 事件触发监听
      */
-    @Override
-    public void userEventTriggered(ChannelHandlerContext ctx, Object event) {
-        // 连接建立，握手完成
-        if (event instanceof WebSocketServerProtocolHandler.HandshakeComplete) {
-            // TODO 修改用户在线状态
-            log.info("--------------------客户端建立连接成功：{}--------------------", ctx);
-        }
-
-        // 心跳检测：30s无心跳触发断开事件
-        else if (event instanceof IdleStateEvent) {
-            IdleStateEvent idleStateEvent = (IdleStateEvent) event;
-            if (idleStateEvent.state() == IdleState.READER_IDLE) {
-                log.info("--------------------30s未检测到心跳，断开WebSocket链接：{}--------------------", ctx);
-                ctx.close(); // 需要手动触发，否则不会主动给下线
-            }
-        }
-    }
+//    @Override
+//    public void userEventTriggered(ChannelHandlerContext ctx, Object event) {
+//        // 连接建立，握手完成
+//        if (event instanceof WebSocketServerProtocolHandler.HandshakeComplete) {
+//            // TODO 修改用户在线状态
+//            log.info("--------------------客户端建立连接成功：{}--------------------", ctx);
+//        }
+//
+//        // 心跳检测：30s无心跳触发断开事件
+//        else if (event instanceof IdleStateEvent) {
+//            IdleStateEvent idleStateEvent = (IdleStateEvent) event;
+//            if (idleStateEvent.state() == IdleState.READER_IDLE) {
+//                log.info("--------------------30s未检测到心跳，断开WebSocket链接：{}--------------------", ctx);
+//                ctx.close(); // 需要手动触发，否则不会主动给下线
+//            }
+//        }
+//    }
 }
