c4b31e9cf9b4f0647fc48f224d226ab6e0ae9d88
徐志斌 <1262254123@qq.com>
2024-01-27 09:45:41 +0800

    fix：新增relation_id生成校验

--- quickchat-common/src/main/java/com/quick/utils/RelationUtil.java
+++ quickchat-common/src/main/java/com/quick/utils/RelationUtil.java
@@ -1,5 +1,9 @@
 package com.quick.utils;
 
+import com.quick.enums.ResponseEnum;
+import com.quick.exception.QuickException;
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * @Author 徐志斌
  * @Date: 2023/12/30 19:05
@@ -11,6 +15,10 @@ public class RelationUtil {
      * 通过参数生成：关系字符串
      */
     public static String generate(String fromId, String toId) {
+        if (StringUtils.isEmpty(fromId) || StringUtils.isEmpty(toId)) {
+            throw new QuickException(ResponseEnum.RELATION_GENERATE_ERROR);
+        }
+
         if (toId.compareTo(fromId) <= 0) {
             return toId + ":" + fromId;
         } else {
