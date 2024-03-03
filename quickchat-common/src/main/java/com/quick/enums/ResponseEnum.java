70b15c97608193a262b9cd7f64b8357e70fe9fe9
徐志斌 <1262254123@qq.com>
2024-03-03 15:52:48 +0800

    feat：JSR303校验引入

--- quickchat-common/src/main/java/com/quick/enums/ResponseEnum.java
+++ quickchat-common/src/main/java/com/quick/enums/ResponseEnum.java
@@ -22,7 +22,7 @@ public enum ResponseEnum {
     IMG_CODE_ERROR(500, "图片验证码输入错误，请重试！"),
     EMAIL_CODE_EXPIRE(500, "邮箱验证码输入错误，请重试！"),
     EMAIL_CODE_ERROR(500, "邮箱验证码输入错误，请重试！"),
-    TOKEN_EXPIRE(500, "Token身份信息已过期，请重新登录！"),
+    TOKEN_EXPIRE(502, "登录身份已过期，请重新登录！"),
     RELATION_GENERATE_ERROR(500, "生成 relation_id 失败，缺少关键信息！"),
     REQUEST_HOLDER_NO_ACCOUNT_ID(500, "全局请求上下文未获取到账号信息！"),
     SEND_MSG_FAST(500, "慢点发消息嘛，别急..."),
