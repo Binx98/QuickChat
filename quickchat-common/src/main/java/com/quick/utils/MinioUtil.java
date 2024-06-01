e09d1e50ab2547956c4a87259b781081d37d8277
徐志斌 <binx19980707@gmail.com>
2024-06-01 09:57:00 +0800

    fix：调整上传文件逻辑

--- quickchat-common/src/main/java/com/quick/utils/MinioUtil.java
+++ quickchat-common/src/main/java/com/quick/utils/MinioUtil.java
@@ -1,9 +1,8 @@
 package com.quick.utils;
 
-import io.minio.GetObjectArgs;
-import io.minio.MinioClient;
-import io.minio.PutObjectArgs;
-import io.minio.RemoveObjectArgs;
+import com.baomidou.mybatisplus.core.toolkit.IdWorker;
+import io.minio.*;
+import io.minio.messages.Item;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -15,6 +14,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.InputStream;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @Author 徐志斌
@@ -41,20 +42,20 @@ public class MinioUtil {
         String fileName = file.getOriginalFilename();
         String[] split = fileName.split("\\.");
         if (split.length > 1) {
-            fileName = split[0] + "_" + System.currentTimeMillis() + "." + split[1];
+            fileName = split[0] + "_" + IdWorker.getId() + "." + split[1];
         } else {
-            fileName = fileName + System.currentTimeMillis();
+            fileName = fileName + IdWorker.getId();
         }
         InputStream in = null;
         try {
             in = file.getInputStream();
-            minioClient.putObject(PutObjectArgs.builder()
+            PutObjectArgs putObjectArgs = PutObjectArgs.builder()
                     .bucket(bucketName)
                     .object(fileName)
                     .stream(in, in.available(), -1)
                     .contentType(file.getContentType())
-                    .build()
-            );
+                    .build();
+            minioClient.putObject(putObjectArgs);
         } finally {
             if (in != null) {
                 in.close();
@@ -126,45 +127,50 @@ public class MinioUtil {
         return url.substring(lastSlashIndex + 1);
     }
 
-//    /**
-//     * 查看文件对象
-//     */
-//    public List<Item> getFileList(String bucketName) throws Exception {
-//        Iterable<Result<Item>> results =
-//                minioClient.listObjects(ListObjectsArgs.builder().bucket(bucketName).build());
-//        List<Item> objectItems = new ArrayList<>();
-//        for (Result<Item> result : results) {
-//            Item item = result.get();
-//            objectItems.add(item);
-//        }
-//        return objectItems;
-//    }
-//
-//    /**
-//     * 创建 bucket
-//     */
-//    public void createBucket(String bucketName) throws Exception {
-//        minioClient.makeBucket(MakeBucketArgs.builder()
-//                .bucket(bucketName)
-//                .build());
-//    }
-//
-//    /**
-//     * 删除 bucket
-//     */
-//    public void deleteBucket(String bucketName) throws Exception {
-//        minioClient.removeBucket(RemoveBucketArgs.builder()
-//                .bucket(bucketName)
-//                .build());
-//    }
-//
-//    /**
-//     * 判断bucket是否存在（不存在则创建）
-//     */
-//    public void existBucket(String name) throws Exception {
-//        boolean exists = minioClient.bucketExists(BucketExistsArgs.builder().bucket(name).build());
-//        if (!exists) {
-//            minioClient.makeBucket(MakeBucketArgs.builder().bucket(name).build());
-//        }
-//    }
+
+    /**
+     * 创建 bucket
+     */
+    public void createBucket(String bucketName) throws Exception {
+        minioClient.makeBucket(
+                MakeBucketArgs.builder()
+                        .bucket(bucketName)
+                        .build()
+        );
+    }
+
+    /**
+     * 删除 bucket
+     */
+    public void deleteBucket(String bucketName) throws Exception {
+        minioClient.removeBucket(
+                RemoveBucketArgs.builder()
+                        .bucket(bucketName)
+                        .build()
+        );
+    }
+
+    /**
+     * 判断bucket是否存在（不存在则创建）
+     */
+    public void existBucket(String name) throws Exception {
+        boolean exists = minioClient.bucketExists(BucketExistsArgs.builder().bucket(name).build());
+        if (!exists) {
+            this.createBucket(name);
+        }
+    }
+
+    /**
+     * 查看文件对象
+     */
+    public List<Item> getFileList(String bucketName) throws Exception {
+        Iterable<Result<Item>> results =
+                minioClient.listObjects(ListObjectsArgs.builder().bucket(bucketName).build());
+        List<Item> objectItems = new ArrayList<>();
+        for (Result<Item> result : results) {
+            Item item = result.get();
+            objectItems.add(item);
+        }
+        return objectItems;
+    }
 }
