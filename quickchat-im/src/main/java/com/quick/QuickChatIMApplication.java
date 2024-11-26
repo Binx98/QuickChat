0f6f9870e872abedf591a39ddd09c1b86b1943e9
徐志斌 <binx19980707@gmail.com>
2024-11-26 10:55:42 +0800

    refactor：项目整体结构调整

--- quickchat-im/src/main/java/com/quick/QuickChatIMApplication.java
+++ quickchat-im/src/main/java/com/quick/QuickChatIMApplication.java
@@ -6,13 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @Slf4j
 @EnableAsync
 @MapperScan("com.quick.mapper")
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class QuickChatIMApplication implements CommandLineRunner {
     @Autowired
     private Environment environment;
